88726fa214b690f5e00d2a847ad144f2a5ace3d0
Pragathi@2020 <pragatisri0005@gmail.com>
2022-11-26 23:46:47 +0530

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,57 @@
 import React from "react";
-
+import "./Footer.scss";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQs</span>
+          <span>Store</span>
+          <span>Pages</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            nisi accusamus consequuntur error fuga quos sint suscipit, soluta
+            quaerat recusandae assumenda sunt. Blanditiis, quia! Ex natus
+            reprehenderit nesciunt quia magni?
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            nisi accusamus consequuntur error fuga quos sint suscipit, soluta
+            quaerat recusandae assumenda sunt. Blanditiis, quia! Ex natus
+            reprehenderit nesciunt quia magni?
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">PragaStore</span>
+          <span className="copyright">
+            &#169; 2023 Copyright. All rights reserved.
+          </span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="pay" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
